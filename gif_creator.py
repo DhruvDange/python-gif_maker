4cc4515add088dd73b05ce668462841c53f1cb18
Dhruv <DhruvDange@github.com>
2021-04-07 21:09:28 +0530

    Update gif_creator.py

--- gif_creator.py
+++ gif_creator.py
@@ -4,15 +4,20 @@ from pygifsicle import optimize
 
 path = os.getcwd()
 frames_path = path + "\\gif_pics"
+frames = []
+duration = 0.2
 
 def gif_generator(gif_path):
-    global path, frames_path
-    writer = imageio.get_writer(gif_path, mode= 'I')
+    global path, frames_path, frames
     for folder, sub_folder, files in os.walk(frames_path):
         for f in files:
             pic_path = frames_path + "\\" + f
             print(pic_path)
-            writer.append_data(imageio.imread(pic_path))
+            frames.append(imageio.imread(pic_path))
+    dur = float(input("Enter duration for each frame (seconds): "))
+    print("Processing...please wait")
+    imageio.mimsave(gif_path, frames, 'GIF', duration = dur)
+    print("Success!")
 
 def welcome_page():
     global name
